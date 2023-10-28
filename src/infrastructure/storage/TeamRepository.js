95db837f5c08779a5792ae33515fe7f59681e753
Adam Hajek <22adamhajek@gmail.com>
2023-10-29 01:06:24 +0200

    mock up all repositories for in memory mocked usage

---
+++ src/infrastructure/storage/TeamRepository.js
@@ -0,0 +1,43 @@
+class TeamRepository {
+  constructor() {
+    // Dummy in-memory storage
+    this.teams = [
+      new Team(1, 'Team Alpha', 'Description for Team Alpha'),
+      new Team(2, 'Team Beta', 'Description for Team Beta'),
+      // Add more teams as necessary
+    ];
+  }
+
+  // Find a team by ID
+  findTeamById(id_team) {
+    return this.teams.find(team => team.id_team === id_team) || null;
+  }
+
+  // Add a new team
+  addTeam(team) {
+    this.teams.push(team);
+    return team;
+  }
+
+  // Delete a team by ID
+  deleteTeamById(id_team) {
+    const teamIndex = this.teams.findIndex(team => team.id_team === id_team);
+    if (teamIndex !== -1) {
+      const [deletedTeam] = this.teams.splice(teamIndex, 1);
+      return deletedTeam;
+    }
+    return null;
+  }
+
+  // Update team details
+  updateTeam(teamToUpdate) {
+    const teamIndex = this.teams.findIndex(team => team.id_team === teamToUpdate.id_team);
+    if (teamIndex !== -1) {
+      this.teams[teamIndex] = teamToUpdate;
+      return teamToUpdate;
+    }
+    return null;
+  }
+}
+
+module.exports = TeamRepository;
